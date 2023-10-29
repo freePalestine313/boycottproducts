8d5ad6a01a2d51ad3ce1a48d9f7e71a5e74e56df
freepalestine <freepalestine123123@proton.me>
2023-10-29 23:38:36 +0530

    json correction added category

--- src/main/java/com/freepalesting313/boycottproducts/enums/Category.java
+++ src/main/java/com/freepalesting313/boycottproducts/enums/Category.java
@@ -1,5 +1,5 @@
 package com.freepalesting313.boycottproducts.enums;
 
 public enum Category {
-    FOOD_OUTLETS, GROCERIES, ENTERTAINMENT, WEARS, OTHERS
+    FOOD_OUTLETS, FOOD_AND_DIARY, GROCERIES, ENTERTAINMENT, WEARS, OTHERS
 }

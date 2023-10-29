d6cc3702dfd0be92ca9092034702b3223922037c
freepalestine <freepalestine123123@proton.me>
2023-10-29 17:08:47 +0530

    project setup

---
+++ src/main/java/com/freepalesting313/boycottproducts/BoycottproductsApplication.java
@@ -0,0 +1,14 @@
+package com.freepalesting313.boycottproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoycottproductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoycottproductsApplication.class, args);
+		System.out.println("Hello world, Free Palestine");
+	}
+
+}
---
+++ src/test/java/com/freepalesting313/boycottproducts/BoycottproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freepalesting313.boycottproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoycottproductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
